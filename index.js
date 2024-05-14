3349633459278b242bffac0ad33bd4ecd04c1e5d
fghjiu87y7 <gs1okcooool@hotmail.com>
2024-05-14 23:10:10 +0700

    aptos commit

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-const titleCase = (str) =>
-  str.toLowerCase().replace(/\b\w/g, (char) => char.toUpperCase());
+function wiggleMaxLength(nums) {
+  if (nums.length === 0) return 0;
+  let up = 1;
+  let down = 1;
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] > nums[i - 1]) {
+      up = down + 1;
+    } else if (nums[i] < nums[i - 1]) {
+      down = up + 1;
+    }
+  }
+  return Math.max(up, down);
+}
