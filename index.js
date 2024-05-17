ba4467a6a0109495fc124dc5fe747ae988fe7fc3
fghjiu87y7 <gs1okcooool@hotmail.com>
2024-05-17 17:20:43 +0700

    demo project

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-function mergeKLists(lists) {
-  if (lists.length === 0) return null;
-  while (lists.length > 1) {
-    const first = lists.shift();
-    const second = lists.shift();
-    const merged = mergeTwoLists(first, second);
-    lists.push(merged);
+function minPathSum(grid) {
+  const rows = grid.length;
+  const cols = grid[0].length;
+  for (let i = 1; i < rows; i++) {
+    grid[i][0] += grid[i - 1][0];
   }
-  return lists[0];
+  for (let j = 1; j < cols; j++) {
+    grid[0][j] += grid[0][j - 1];
+  }
+  for (let i = 1; i < rows; i++) {
+    for (let j = 1; j < cols; j++) {
+      grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]);
+    }
+  }
+  return grid[rows - 1][cols - 1];
 }
